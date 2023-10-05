d4ee97a6347e893df6e6fb0d38083b530331a546
rockij <rockij83@nate.com>
2023-10-05 23:30:54 +0900

    reset

--- sample/src/api/platform/dashboard.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-/**
- * 리스트 조회 ( category )
- * 
- */
-export const _getCategoryList = async (params) => {
-    return await 'test !!!';
-    // return await $api.post('', params);
-};
